b3d49bfc60604e8cdf4f67bcf7e97169fa1ce892
planhorror <nmtxma2004@gmail.com>
2025-04-26 20:59:30 +0700

    Customer and Company refactor

--- backend/src/customer/customer.controller.ts
+++ backend/src/customer/customer.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Patch,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { GetUser, Roles } from 'src/common/decorators';
+import { Role } from 'src/common/enums';
+import { RolesGuard } from 'src/common/guards/authorized.guard';
+import { CustomerService } from './customer.service';
+import { Customer } from './entity/customer.entity';
+import { CustomerUpdateDto } from 'src/common/dtos';
 
 @Controller('customer')
-export class CustomerController {}
+@UseGuards(AuthGuard(), RolesGuard)
+@Roles(Role.CUSTOMER)
+export class CustomerController {
+  constructor(private customerService: CustomerService) {}
+
+  @Get()
+  async getProfile(@GetUser() customer: Customer): Promise<Customer> {
+    return await this.customerService.getCustomerById(customer.id);
+  }
+
+  @Patch()
+  async updateProfile(
+    @GetUser() customer: Customer,
+    @Body() customerUpdateDto: CustomerUpdateDto,
+  ): Promise<Customer> {
+    return await this.customerService.updateCustomerService(
+      customer,
+      customerUpdateDto,
+    );
+  }
+
+  @Delete()
+  async deleteProfile(@GetUser() customer: Customer): Promise<void> {
+    return await this.customerService.deleteCustomerService(customer);
+  }
+}
