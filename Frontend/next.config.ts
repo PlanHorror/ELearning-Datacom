9c00b6984902ad8b90b5aa61226000c7fe551cce
PAKouzo <dohieuxk14@gmail.com>
2025-05-16 06:54:13 +0700

    handle conflict

--- Frontend/next.config.ts
+++ Frontend/next.config.ts
@@ -3,17 +3,13 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
   images: {
-<<<<<<< HEAD
-    domains: ["localhost","3.106.244.62"],
+    domains: ["res.cloudinary.com","3.106.244.62"],
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
-=======
-    domains: ["res.cloudinary.com"],
->>>>>>> dc3f2112632999b4d3f86beea0a62f697bc41197
   },
 };
 
